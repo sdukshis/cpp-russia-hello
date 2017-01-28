fe45cd5081ef87d21cdf77af0851673d8098e112
Pavel Filonov <filonovpv@gmail.com>
2017-01-28 14:43:14 +0300

    fix cmake build cmd

--- .travis/run.sh
+++ .travis/run.sh
@@ -13,5 +13,5 @@ fi
 mkdir build && cd build
 conan install .. -s build_type=$BUILD_TYPE --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
-cmake --build . -VERBOSE=1
+cmake --build . -- VERBOSE=1
 ctest -V
